e92236c960fbd834308af172998327615f0cb19e
vanshita shah <vanshita.s@simformsolutions.com>
2023-06-09 10:03:37 +0530

    Added routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,28 @@ import Applicants from "./Pages/Applicants/Applicants";
 import AllJobs from "./Pages/AllJobs/AllJobs";
 import AddJob from "./Pages/AddJob/AddJob";
 import Applications from "./Pages/Applications/Applications";
+import { Suspense, useEffect } from "react";
+import { auth } from "./Firebase/firebase";
+import Profile from "./Pages/CompleteProfile/Profile";
+import EditProfile from "./Pages/Editprofile/EditProfile";
 
 const App = () => {
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      console.log(user?.email);
+    });
+  }, []);
   return (
     <>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/complete-profile" element={<Profile />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
         <Route path="/all-jobs" element={<AllJobs />} />
         <Route path="/add-job" element={<AddJob />} />
+        <Route path="/edit-job" element={<AddJob type="edit" />} />
         <Route path="/applicants" element={<Applicants />} />
         <Route path="/applications" element={<Applications />} />
       </Routes>
