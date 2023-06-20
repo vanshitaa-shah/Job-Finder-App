3b2890e83ede9ca43da9ce7c092055345e447b0b
vanshita shah <vanshita.s@simformsolutions.com>
2023-06-20 16:59:27 +0530

    Added applications and applicants array properties in users and jobs firebase documents

--- src/components/Forms/AddJob/AddJobComponent.tsx
+++ src/components/Forms/AddJob/AddJobComponent.tsx
@@ -33,12 +33,15 @@ const AddJobComponent = ({ type }: { type?: string }) => {
       const jobData: Job = location.state.jobData;
       const id = jobData.id;
       if (editValues) {
-        await jobServices.updateJob(id, values);
+        if (id) await jobServices.updateJob(id, values);
       }
       navigate("/all-jobs");
     } else {
-      const jobData = { ...values, providerEmail: email };
-      await jobServices.addjob(jobData);
+      if (email) {
+        const jobData = { ...values, providerEmail: email };
+        const applicants = [] as string[];
+        await jobServices.addjob({ ...jobData, applicants: applicants });
+      }
 
       navigate("/all-jobs");
     }
