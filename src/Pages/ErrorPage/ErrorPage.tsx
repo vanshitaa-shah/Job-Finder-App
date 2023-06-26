b55454f12b3f5e632754a93b5a85a6404987907c
vanshita shah <vanshita.s@simformsolutions.com>
2023-06-26 15:56:47 +0530

    Added Error Page

--- src/Pages/ErrorPage/ErrorPage.tsx
+++ src/Pages/ErrorPage/ErrorPage.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import Styles from "./ErrorPage.module.css";
 
 const ErrorPage = () => {
   return (
-    <div>
-      NOT FOUND
-    </div>
-  )
-}
+    <div className={Styles.errorPage}>
+      <div className={Styles.container}>
+        <Typography variant="h5">404 Page Not Found!!</Typography>
+        <Typography variant="h5">
+          The requested page could not be found.
+        </Typography>
 
-export default ErrorPage
+        <Link to="/">
+          <Button variant="contained">Go to Home</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
